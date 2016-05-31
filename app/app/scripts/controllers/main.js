6f7ab38d620cbe45182423c42101c2c40a5d6f9d
alexbores <boresrivera@gmail.com>
2016-05-31 15:20:45 -0400

    alex2

---
+++ app/app/scripts/controllers/main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name centroEureApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the centroEureApp
+ */
+angular.module('centroEureApp')
+  .controller('MainCtrl', function () {
+  	var vm = this;
+  });
+
+  
